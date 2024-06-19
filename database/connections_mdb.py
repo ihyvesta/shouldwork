e2c09a1e0eaa05aa882215b0c560a10ebecafc2c
ihyvesta <160210388+ihyvesta@users.noreply.github.com>
2024-06-19 22:40:37 +0100

    Update connections_mdb.py

--- database/connections_mdb.py
+++ database/connections_mdb.py
@@ -6,8 +6,8 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
-myclient = pymongo.MongoClient(mongodb+srv://ihyvesta:ihyvesta@cluster0.z9rsozg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0)
-mydb = myclient[ihyvesta]
+myclient = pymongo.MongoClient(DATABASE_URI)
+mydb = myclient[DATABASE_NAME]
 mycol = mydb['CONNECTION']  
 
 myclient2 = pymongo.MongoClient(SECONDDB_URI)
